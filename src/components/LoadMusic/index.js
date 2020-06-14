a44f5e4fa127032afce193b47c32889ed67b71d0
Alex <suarez9093@hotmail.com>
2020-06-13 20:12:03 -0600

    able to add a song to state

--- src/components/LoadMusic/index.js
+++ src/components/LoadMusic/index.js
@@ -3,15 +3,12 @@ import { AppContext } from '../../context';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 const LoadMusic = (props) => {
-    const { modal, toggle } = useContext(AppContext);
+    const { modal, toggle, handleChange, setSource, song } = useContext(AppContext);
 
     const {
         buttonLabel,
         className
     } = props;
-
-    // const [modal, setModal] = useState(false);
-    // const toggle = () => setModal(!modal);
 
     return (
         <div>
@@ -22,15 +19,35 @@ const LoadMusic = (props) => {
                     <Form>
                         <FormGroup>
                             <Label for="songTitle">Song Name</Label>
-                            <Input type="text" name="song" id="songTitle" placeholder="Song Name" required />
+                            <Input
+                                type="text"
+                                name="song"
+                                id="songTitle"
+                                placeholder="Song Name"
+                                value={song.song}
+                                onChange={handleChange}
+                            />
                         </FormGroup>
                         <FormGroup>
                             <Label for="Artist">Artist</Label>
-                            <Input type="text" name="artist" id="artist" placeholder="Artist Name" required />
+                            <Input
+                                type="text"
+                                name="artist"
+                                id="artist"
+                                placeholder="Artist Name"
+                                value={song.artist}
+                                onChange={handleChange}
+                            />
                         </FormGroup>
                         <FormGroup>
                             <Label for="songLink">Link to song</Label>
-                            <Input type="text" name="songLink" id="song-link" placeholder="https://www.location.com" required />
+                            <Input
+                                type="text"
+                                name="link"
+                                id="song-link"
+                                value={song.link}
+                                onChange={handleChange}
+                                placeholder="https://www.location.com" />
                         </FormGroup>
                         {/* <FormGroup>
                             <Label for="exampleText">Text Area</Label>
@@ -38,7 +55,7 @@ const LoadMusic = (props) => {
                         </FormGroup> */}
 
 
-                        <Button>Submit</Button>
+                        <Button onClick={setSource}>Submit</Button>
                     </Form>
 
                 </ModalBody>
--- src/context.js
+++ src/context.js
@@ -2,20 +2,37 @@ import React, { useState, createContext } from "react"
 const AppContext = createContext()
 
 function AppContextProvider(props) {
-    const [musicSource, setMusicSource] = useState()
-
+    const [songArray, setSongArray] = useState([])
+    const [song, setSong] = useState({ song: "", artist: "", link: "" })
     const [modal, setModal] = useState(false);
     function toggle() {
         setModal(!modal);
     };
 
+    function handleChange(e) {
+        const { name, value } = e.target
+        setSong(prevState => ({
 
-    function setSource(source) {
-        setMusicSource(source)
+            ...prevState, [name]: value
+        }))
+    };
+
+    function setSource() {
+        // let toLower = song.song.trim().toLowerCase()
+        // console.log(toLower)
+        const songToLower = {
+            song: song.song.trim().toLowerCase(),
+            artist: song.artist.trim().toLowerCase(),
+            link: song.link.trim().toLowerCase(),
+        }
+        setSongArray([...songArray, songToLower])
+        setSong({ song: "", artist: "", link: "" })
+        setModal(!modal)
     }
-
+    console.log('song:', song)
+    console.log('songArray:', songArray)
     return (
-        <AppContext.Provider value={{ musicSource, setSource, modal, toggle }}>
+        <AppContext.Provider value={{ setSong, setSource, modal, song, handleChange, toggle }}>
             {props.children}
         </AppContext.Provider>
     )
