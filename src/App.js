cc2e9e2d97261c52b6c52b8101d42db0571a66b5
Alex <suarez9093@hotmail.com>
2020-06-14 19:54:27 -0600

    added some basic styling to the container

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
-// import LoadLyricsButton from "./components/Buttons/LoadLyricsButton";
-// import LoadMusicButton from "./components/Buttons/LoadMusicButton";
+import LoadLyricsButton from "./components/Buttons/LoadLyricsButton";
+import LoadMusicButton from "./components/Buttons/LoadMusicButton";
 import LyricContainer from "./components/LyricContainer/LyricContainer";
 // import MusicPlayer from "./components/MusicPlayer/MusicPlayer";
 
 function App() {
   return (
     <Container>
-      <Row>
-        <Col />
-        <Col />
-      </Row>
+      <br />
+
       <Row>
         <Col />
         <Col md="6">
           <LyricContainer />
         </Col>
         <Col />
+      </Row>
+      <Row>
+        <Col />
+        <Col md="6">
+          <LoadMusicButton />
+
+        </Col>
+        <Col><LoadLyricsButton /></Col>
       </Row>
       <Row>
         <Col />
--- src/components/LyricContainer/LyricContainer.js
+++ src/components/LyricContainer/LyricContainer.js
@@ -1,26 +1,26 @@
 import React, { useContext, useEffect } from "react";
-import { Jumbotron } from "reactstrap";
 import { AppContext } from "../../context";
 import LoadLyricsButton from "../Buttons/LoadLyricsButton";
 import LoadMusicButton from "../Buttons/LoadMusicButton";
+import './style.css';
+
 function LyricContainer() {
   const { displaySongLyrics, songData } = useContext(AppContext);
 
 
   useEffect(() => {
-    document.getElementsByClassName('rabbit-lyrics').innerHTML = displaySongLyrics
+    document.getElementById('rabbit-lyrics').innerHTML = displaySongLyrics
   }, [displaySongLyrics])
   return (
     <>
-      <Jumbotron>
-        <h1 className="display-3">Lyrics</h1>
+      <div className="lyric-grid-container">
+        <h1 className="display-3">{songData.title}</h1>
         <hr className="my-2" />
-        <div className="rabbit-lyrics" data-media="#audioFile" data-height="500" data-alignment="center">{displaySongLyrics}</div>
+        <div id="rabbit-lyrics" className="rabbit-lyrics" data-media="#audioFile" data-height="500" data-alignment="center">{displaySongLyrics}</div>
 
-        <LoadMusicButton />
-        <br />
-        <LoadLyricsButton />
-      </Jumbotron>
+        <h6>{songData.artist}</h6>
+      </div>
+      <br></br>
       <audio id="audioFile" src={songData.link} controls>
         Your browser does not support the audio element.
       </audio>
