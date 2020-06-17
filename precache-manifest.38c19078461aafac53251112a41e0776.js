859b8ec96d83366a7be8d2ade5e6568e0cb532b5
Alex <suarez9093@hotmail.com>
2020-06-16 21:14:56 -0600

    Updates

---
+++ precache-manifest.38c19078461aafac53251112a41e0776.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c00b7d9e198c09cdc6a1e5f45664958f",
+    "url": "/music-player/index.html"
+  },
+  {
+    "revision": "4a95d09f68a2177824d5",
+    "url": "/music-player/static/css/2.37620a9f.chunk.css"
+  },
+  {
+    "revision": "5ea983ba74607ba6fb8f",
+    "url": "/music-player/static/css/main.216fea96.chunk.css"
+  },
+  {
+    "revision": "4a95d09f68a2177824d5",
+    "url": "/music-player/static/js/2.36cbc8b0.chunk.js"
+  },
+  {
+    "revision": "94a3bfaba5b0bd866a3e56687327b9ad",
+    "url": "/music-player/static/js/2.36cbc8b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea983ba74607ba6fb8f",
+    "url": "/music-player/static/js/main.cc07423b.chunk.js"
+  },
+  {
+    "revision": "f1e930d8d95e9708f2ce",
+    "url": "/music-player/static/js/runtime-main.c923f3ec.js"
+  }
+]);
